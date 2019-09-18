1e4f2fe7b3fd31969d3993a6f2da18e199a41b72
Natan Danous <natan.danous@gmail.com>
2019-09-18 11:45:04 +0200

    Simple regex

---
+++ td1/README.md
@@ -0,0 +1,5 @@
+# TD1
+
+## Présentation
+
+Chaque article est issu d'une rubrique et d'un bulletin. Il va falloir construire les tables inverse (pour chaque article, avoir toutes les infos utiles, c.f. le cours).
---
+++ td1/main.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+
+# Workflow : cat OLD_BULLETINS_LO17/67068.htm | perl main.pl > index.xml
+
+# Check numéro class is in all files.
+# grep 'style32"' OLD_BULLETINS_LO17/*.htm | uniq -c | wc -l
+
+# @ls for all the files
+
+while (<>) {
+    if ( $_ =~ /class="style32"/ ) {
+        print "$ARGV $. $_";
+    }
+}
