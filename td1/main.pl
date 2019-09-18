3266772a575a10bf9136f2fd1fd73e84d1cf765b
Natan Danous <natan.danous@gmail.com>
2019-09-18 19:56:02 +0200

    Add logging

--- td1/main.pl
+++ td1/main.pl
@@ -11,15 +11,22 @@ my @files = `ls OLD_BULLETINS_LO17/*.htm`;
 
 my $output = "<corpus>";
 
-my $filecount = 0;
+my %log = ( 
+    "fichiers" => 0,
+    "numero" => 0,
+    "date" => 0,
+    "rubrique" => 0,
+    "titre" => 0,
+    "texte" => 0,
+    "images" => 0,
+    "contact" => 0
+);
+
 foreach my $filename(@files) {
     open my $filecontent, $filename or die "Could not open $filename: $!";
-
-    $filecount++;
     
-    my $found = 0;
-
     $output = $output."<bulletin><fichier>$filename</fichier>";
+    ++$log{fichiers};
 
     while (my $line = <$filecontent>) {
         # Replace html entities with correct accents
@@ -50,37 +57,40 @@ foreach my $filename(@files) {
             $line =~ s/&#21;/--/g;
         }
 
-        # Get number and date
+        # Get numero and date
         if ( $line =~ /class="style32">BE France (\d+).*class="style42">.*;(\d{1,2}\/\d{1,2}\/\d{4})<\/span><\/p><\/td>/ ) {
-            $found = 1;
             my $date = $2;
             if (length $date == 9) {
                 $date = "0".$date;
             }
             $output = $output."<numero>$1</numero><date>$date</date>";
+            ++$log{numero};
+            ++$log{date};
         } 
 
         # Get rubrique
         if ( $line =~ /<span class="style42">(([A-zÀ-ÿ]+ )*([A-zÀ-ÿ]*))<br>/ ) {
-            $found = 1; 
             $output = $output."<rubrique>$1</rubrique>";
+            ++$log{rubrique};
         }
         
-        # Get title
+        # Get titre
         if ( $line =~ /<span class="style17">(.*)<\/span>/ ) {
-            $found = 1;
             $output = $output."<titre>$1</titre>";
+            ++$log{titre};
         } 
 
-        # Get text 
+        # Get texte
+        # $output = $output."<texte>";
+        # if ( $line =~ /<span class="style17">(.*)<\/span>/ ) {
+        #     $output = $output."<titre>$1</titre>";
+        #     ++$log{texte};
+        # }
+        # $output = $output."</texte>";
 
         # Get images
 
         # Get contact
-    }
-
-    if ($found == 0) {
-        print "Not found in $filename \n";
     }
 
     $output = $output."</bulletin>";
@@ -94,3 +104,13 @@ unless(open FILE, ">corpus.xml") {
 
 print FILE $output;
 close FILE;
+
+print "Log :\n";
+foreach my $key (keys %log) {
+    my $msg = "- $key : $log{$key}";
+    if ($log{$key} != $log{fichiers}) {
+        $msg = $msg." --> erreur";
+    }
+    $msg = $msg."\n";
+    print $msg;
+}
