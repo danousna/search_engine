904758c0c2a60a9c64c8d6e1bc68435b2ec481ca
Natan Danous <natan.danous@gmail.com>
2019-09-18 11:52:20 +0200

    Match number and date

--- td1/main.pl
+++ td1/main.pl
@@ -10,7 +10,7 @@ use warnings;
 # @ls for all the files
 
 while (<>) {
-    if ( $_ =~ /class="style32"/ ) {
-        print "$ARGV $. $_";
+    if ( $_ =~ /class="style32">BE France (\d+).*class="style42">.*(\d{2}\/\d{2}\/\d{4})<\/span><\/p><\/td>/ ) {
+        print "$1 $2";
     }
 }
