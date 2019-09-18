406ad8e4a1e5a04cb9e45efd7b1f6a41bb81ccdf
Natan Danous <natan.danous@gmail.com>
2019-09-18 12:34:32 +0200

    Generate corpus and handle date format

--- td1/main.pl
+++ td1/main.pl
@@ -2,31 +2,49 @@
 use strict;
 use warnings;
 
-# Workflow : cat OLD_BULLETINS_LO17/67068.htm | perl main.pl > index.xml
+# Workflow : perl main.pl
 
 # Check numéro class is in all files.
 # grep 'style32">BE France' OLD_BULLETINS_LO17/*.htm | uniq -c | wc -l
 
-# @ls for all the files
-
 my @files = `ls OLD_BULLETINS_LO17/*.htm`;
 
-my $i = 0;
+my $output = "<corpus>";
 
+my $filecount = 0;
 foreach my $filename(@files) {
     open my $filecontent, $filename or die "Could not open $filename: $!";
 
-    $i++;
+    $filecount++;
     my $found = 0;
 
+    $output = $output."<bulletin><fichier>$filename</fichier>";
+
     while (my $line = <$filecontent>) {
-        if ( $line =~ /class="style32">BE France (\d+).*class="style42">.*(\d{1,2}\/\d{1,2}\/\d{4})<\/span><\/p><\/td>/ ) {
-            print "$i : $1 $2 \n";
+        if ( $line =~ /class="style32">BE France (\d+).*class="style42">.*;(\d{1,2}\/\d{1,2}\/\d{4})<\/span><\/p><\/td>/ ) {
+            my $number = $1;
+            my $date = $2;
+
+            if (length $date == 9) {
+                $date = "0".$date;
+            }
+            $output = $output."<numero>$number</numero><date>$date</date>";
             $found = 1;
         }
     }
 
     if ($found == 0) {
-        print "Not found in $filename";
+        print "Not found in $filename \n";
     }
+
+    $output = $output."</bulletin>";
 }
+
+$output = $output."</corpus>";
+
+unless(open FILE, ">corpus.xml") {
+    die "\nUnable to open corpus.xml\n";
+}
+
+print FILE $output;
+close FILE;
