1b04e8c97b3c3b8d458f5f9708b8c282e506ec9f
Natan Danous <natan.danous@gmail.com>
2019-09-18 12:06:44 +0200

    Loop through all files

--- td1/main.pl
+++ td1/main.pl
@@ -5,12 +5,28 @@ use warnings;
 # Workflow : cat OLD_BULLETINS_LO17/67068.htm | perl main.pl > index.xml
 
 # Check numéro class is in all files.
-# grep 'style32"' OLD_BULLETINS_LO17/*.htm | uniq -c | wc -l
+# grep 'style32">BE France' OLD_BULLETINS_LO17/*.htm | uniq -c | wc -l
 
 # @ls for all the files
 
-while (<>) {
-    if ( $_ =~ /class="style32">BE France (\d+).*class="style42">.*(\d{2}\/\d{2}\/\d{4})<\/span><\/p><\/td>/ ) {
-        print "$1 $2";
+my @files = `ls OLD_BULLETINS_LO17/*.htm`;
+
+my $i = 0;
+
+foreach my $filename(@files) {
+    open my $filecontent, $filename or die "Could not open $filename: $!";
+
+    $i++;
+    my $found = 0;
+
+    while (my $line = <$filecontent>) {
+        if ( $line =~ /class="style32">BE France (\d+).*class="style42">.*(\d{1,2}\/\d{1,2}\/\d{4})<\/span><\/p><\/td>/ ) {
+            print "$i : $1 $2 \n";
+            $found = 1;
+        }
+    }
+
+    if ($found == 0) {
+        print "Not found in $filename";
     }
 }
