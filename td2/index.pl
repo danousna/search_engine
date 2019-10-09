e0e322776e79bbbcd07e442affb431ea9eab4bfc
Leo Przewlocki <leo.przewlocki@etu.utc.fr>
2019-10-09 18:27:09 +0200

    test index.pl in index.txt

--- td2/index.pl
+++ td2/index.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
-$balise = shift(@ARGV);
+# perl index.pl corpus_lemme.xml > index.txt
+
+# $balise = shift(@ARGV);
+$balise = "texte";
 print STDERR "création d'index pour la balise <$balise>...\n";
 
 while (<>) {
@@ -8,28 +11,31 @@ while (<>) {
 		$fichier="\t".$1;
 	} elsif (/<numero>(.+)<\/numero>/) {
 		$numero="\t".$1;
+    } elsif (/<rubrique>(.*)<\/rubrique>/) {
+        $rubrique="\t".$1;
 	} elsif (/<$balise>(.*)<\/$balise>/) {
 		$index = $1;
 	}
-	if ($index && $fichier && numero) {
+	if ($index && $fichier && numero && rubrique) {
         if ($balise eq 'date') {
             @date = split(/\//,$index);
             $jour = shift(@date);
-#            print("jour $jour\n");
+            # print("jour $jour\n");
             $mois = "\t".shift(@date);
-#            print("mois $mois\n");
+            # print("mois $mois\n");
             $annee = "\t".shift(@date);
-#            print("annee $annee\n");
+            # print("annee $annee\n");
             $index=$jour.$mois.$annee
-            }
-            $tabindex{$index} .= "$fichier$numero";
-            }
-		$index="";
-	}
+        }
+        $tabindex{$index} .= "$rubrique$fichier$numero";
+    }
+	$index="";
+}
 
 
 @cleindex = keys %tabindex ;
 foreach $index (@cleindex) {
 	print $index.$tabindex{$index}."\n";
+    # chop the texte to have the index for each word ?
 }
 
