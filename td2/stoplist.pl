a64beb5da019f03136f0c891be3f6fe128a16026
Natan Danous <natan.danous@gmail.com>
2019-10-09 11:28:18 +0200

    Generate successor

--- td2/stoplist.pl
+++ td2/stoplist.pl
@@ -3,11 +3,11 @@
 # perl stoplist.pl coefficients.txt > stoplist.txt
 
 my %stoplist = ();
-my $threshold = 1.5; # arbitraire af
+my $threshold = 1.3; # arbitraire af
 
 while (<>) {
     if ( /(.*)\t(.*)\t(.*)/i ) {
-        if ( $3 <= $threshold ) {
+        if ( $3 <= $threshold ) { # reverse to make golist
             $stoplist{$2} = 1;
         }
     }
