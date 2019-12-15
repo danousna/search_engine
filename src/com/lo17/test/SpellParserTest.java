636e2e057f04e2ab6c98fef23833431fa7c6f327
Natan Danous <natan.danous@gmail.com>
2019-12-15 18:26:28 +0100

    Better tests

--- src/com/lo17/test/SpellParserTest.java
+++ src/com/lo17/test/SpellParserTest.java
@@ -13,9 +13,17 @@ public class SpellParserTest {
     Map<String, String> cases = new HashMap<String, String>();
 
     public SpellParserTest() {
-        Map<String, String> structureLexicon = parser.getStructure();
+        Map<String, String> infoLexicon = new HashMap<String, String>();
 
-        for (Map.Entry<String, String> pair : structureLexicon.entrySet()) {
+        // The infos we can query from the DB.
+        infoLexicon.put("page", "fichier");
+        infoLexicon.put("article", "fichier");
+        infoLexicon.put("articles", "fichier");
+        infoLexicon.put("fichiers", "fichier");
+        infoLexicon.put("rubrique", "rubrique");
+        infoLexicon.put("rubriques", "rubrique");
+
+        for (Map.Entry<String, String> pair : infoLexicon.entrySet()) {
             cases.put(
                     String.format("Je veux les %s qui parlent de hexa et qui datent de 2012.", pair.getKey()),
                     String.format("vouloir %s mot hexagon et date 2012", pair.getValue())
@@ -29,6 +37,34 @@ public class SpellParserTest {
                     String.format("vouloir %s mot innovation ou hexagon date janvier 2012 ou février 2013", pair.getValue())
             );
             cases.put(
+                    String.format("Je veux les %s qui datent du 22/03/2012.", pair.getKey()),
+                    String.format("vouloir %s date 22/03/2012", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s et les rubriques datant d'avant 2015.", pair.getKey()),
+                    String.format("vouloir %s et rubrique date avant 2015", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s datant d'avant 2017 et d'après 2013.", pair.getKey()),
+                    String.format("vouloir %s date avant 2017 et après 2013", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s datant entre le 12/03/2012 et le 06/05/2013.", pair.getKey()),
+                    String.format("vouloir %s date entre 12/03/2012 et 06/05/2013", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s de la rubrique news.", pair.getKey()),
+                    String.format("vouloir %s rubrique news", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s et les dates des articles parlant de respect.", pair.getKey()),
+                    String.format("vouloir %s et date fichier mot respect", pair.getValue())
+            );
+            cases.put(
+                    String.format("Je veux les %s de la rubrique focus de février 2014.", pair.getKey()),
+                    String.format("vouloir %s rubrique focus février 2014", pair.getValue())
+            );
+            cases.put(
                     String.format("Combien d'%s parlent de hexa ou d'innovation et datent du 01/01/2012 ?", pair.getKey()),
                     String.format("combien %s mot hexagon ou innovation et date 01/01/2012", pair.getValue())
             );
@@ -38,7 +74,7 @@ public class SpellParserTest {
     @Test
     public final void testProcess() {
         for (Map.Entry<String, String> pair : cases.entrySet()) {
-            assertEquals(parser.process(pair.getKey()), pair.getValue());
+            assertEquals(pair.getValue(), parser.process(pair.getKey()));
         }
     }
 }
